1f29cf3f519b562bd79a7883be4dfea8b77d5831
ashwithans <ashwithans@gmail.com>
2024-01-10 15:07:51 +0530

    Commit

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ using ConsoleAppAssignmentDay4;
 
 SortedList<string, Product> products = new SortedList<string, Product>();
 
-products.Add("Product1", new Product { ProductId = 1, PName = "Moisturizing Cream", PPrice = 0, PBrand = "Nivea", MfgDate = new DateTime(2022, 11, 12), ExpDate = DateTime.Now });
-products["Product2"] = new Product { ProductId = 2, PName = "Lipstick", PPrice = 10.5m, PBrand = "Maybelline", MfgDate = new DateTime(2022, 11, 12), ExpDate = DateTime.Now };
-products["Product3"] = new Product { ProductId = 3, PName = "Facial Mask", PPrice = 20.75m, PBrand = "L'Oréal", MfgDate = new DateTime(2022, 11, 12), ExpDate = DateTime.Now };
-products["Product4"] = new Product { ProductId = 4, PName = "Eyeliner", PPrice = 15.25m, PBrand = "Revlon", MfgDate = new DateTime(2023, 11, 12), ExpDate = DateTime.Now };
-products["Product5"] = new Product { ProductId = 5, PName = "Shampoo", PPrice = 30.0m, PBrand = "Pantene", MfgDate = new DateTime(2023, 11, 12), ExpDate = DateTime.Now };
-products["Product6"] = new Product { ProductId = 6, PName = "Perfume", PPrice = 18.0m, PBrand = "Calvin Klein", MfgDate = new DateTime(2021, 11, 12), ExpDate = DateTime.Now };
+products.Add("Product1", new Product { ProductId = 1, PName = "Moisturizing Cream", PPrice = 350, PBrand = "Nivea", MfgDate = new DateTime(2022, 11, 12), ExpDate = DateTime.Now });
+products["Product2"] = new Product { ProductId = 2, PName = "Lipstick", PPrice =999, PBrand = "Maybelline", MfgDate = new DateTime(2022, 11, 12), ExpDate = DateTime.Now };
+products["Product3"] = new Product { ProductId = 3, PName = "Facial Mask", PPrice = 150, PBrand = "L'Oréal", MfgDate = new DateTime(2022, 11, 12), ExpDate = DateTime.Now };
+products["Product4"] = new Product { ProductId = 4, PName = "Eyeliner", PPrice = 250m, PBrand = "Revlon", MfgDate = new DateTime(2023, 11, 12), ExpDate = DateTime.Now };
+products["Product5"] = new Product { ProductId = 5, PName = "Shampoo", PPrice = 180, PBrand = "Pantene", MfgDate = new DateTime(2023, 11, 12), ExpDate = DateTime.Now };
+products["Product6"] = new Product { ProductId = 6, PName = "Perfume", PPrice = 4500, PBrand = "Calvin Klein", MfgDate = new DateTime(2021, 11, 12), ExpDate = DateTime.Now };
+products["Product7"] = new Product { ProductId = 7, PName = "Hair mask", PPrice = 650, PBrand = "Tressemme", MfgDate = new DateTime(2021, 11, 12), ExpDate = DateTime.Now };
 
 Console.WriteLine("ID\tName\t\t\tBrand\t\tMfg Date");
 
